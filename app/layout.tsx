ca2e0bc642fc7ba278dbcb14a5ba702a6bbb54a6
KhalidTheCoder <khalidahsan2407@gmail.com>
2026-06-24 15:54:01 +0600

    feat: integrate shadcn/ui components, universal card and configure Tailwind CSS theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import Footer from "@/components/shared/Footer";
 import type { Metadata } from "next";
-import { Geist_Mono, Open_Sans } from "next/font/google";
+import { Geist_Mono, Open_Sans, Geist } from "next/font/google";
 import Navbar from "@/components/shared/Navbar";
 import TopNavLinks from "@/components/shared/TopNavLinks";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -28,7 +31,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${openSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", openSans.variable, geistMono.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full bg-background text-foreground">
         <div className="flex min-h-full flex-col">
