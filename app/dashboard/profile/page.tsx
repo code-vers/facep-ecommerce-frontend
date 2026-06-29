6c3ba67cae6c4eda4f1ceb9eafb0ec05e8c39b4a
FyslAhmd <info.faysal.32@gmail.com>
2026-06-29 15:06:40 +0600

    feat: implement profile settings form component and dashboard profile page

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,9 @@
+import ProfileSettingsForm from '@/components/dashboard/profile/ProfileSettingsForm';
+
+export default function DashboardProfilePage() {
+  return (
+    <div className='flex w-full flex-col items-center px-[45px] py-[36px] bg-white'>
+      <ProfileSettingsForm />
+    </div>
+  );
+}
