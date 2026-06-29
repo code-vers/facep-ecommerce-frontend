dbfdbbb39d2f31353d838a9b8c0875fae00c49ac
FyslAhmd <info.faysal.32@gmail.com>
2026-06-29 14:59:21 +0600

    feat: create storefront management page and configuration form component

---
+++ app/dashboard/storefront/page.tsx
@@ -0,0 +1,9 @@
+import StorefrontForm from '@/components/dashboard/storefront/StorefrontForm';
+
+export default function DashboardStoreManagementPage() {
+  return (
+    <div className='flex w-full flex-col items-center px-[45px] py-[36px] bg-white'>
+      <StorefrontForm />
+    </div>
+  );
+}
